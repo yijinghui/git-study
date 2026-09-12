2a66ca87f21d124081e7e0366d04d17c8247599d
Hhh <480395924@qq.com>
2026-09-12 12:22:03 +0800

    完成用户登录的接口开发

---
+++ src/main/java/com/ethan/gitstudy/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.ethan.gitstudy.controller;
+
+
+//@RestController
+public class UserController {
+
+    public String login(String username, String password){
+        return "登录成功！";
+    }
+}
