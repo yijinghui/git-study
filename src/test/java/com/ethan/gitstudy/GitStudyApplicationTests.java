7561f584a25f7fe11afe8da2f01e287fe4819037
Hhh <480395924@qq.com>
2026-09-12 12:14:01 +0800

    添加完整项目目录

---
+++ src/test/java/com/ethan/gitstudy/GitStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethan.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
