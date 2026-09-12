ab43a24c31064c321f99047cf6b1576c687bb30d
Hhh <480395924@qq.com>
2026-09-12 13:49:28 +0800

    提交了一段代码

--- src/main/java/com/ethan/gitstudy/controller/UserController.java
+++ src/main/java/com/ethan/gitstudy/controller/UserController.java
@@ -23,4 +23,8 @@ public class UserController {
     public String update(String username, String password){
         return "更新成功！";
     }
+
+    public String bug(){
+        return "这是一段错误的代码";
+    }
 }
